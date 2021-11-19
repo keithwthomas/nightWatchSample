ee5c46caafa960f59924a8f01af2a4d196c5b885
Keith <12878264+keithwthomas@users.noreply.github.com>
2021-11-19 10:20:05 -0800

    cheap workaround to make progress

--- tests/secondTest.js
+++ tests/secondTest.js
@@ -6,13 +6,31 @@ module.exports = {
 
         const langDropdownSelector = '#lr_button';
         const langDropdownValueSelector = '.goog-menuitem[value="lang_it"]';
+        const lastUpdateDropdownSelector = '#as_qdr_button';
+        const lastUpdateDropdownValueSelector = '.goog-menuitem[value="m"]';
+        const submitButtonSelector = '.jfk-button[type="submit"]';    
+        
+        const resultsPageQuerySelector = '#searchform input[name="q"]';
+        const resultsPageLangSelector =  'body';   //'"//div[contains(@class, 'KTBKoe') and text()=" Search Itaian pages"]';
+        const resultsPageLastUpdateSelector = 'body';      //'div.KTBKoe[value="Past month"]';
 
         browser
             .url('https://www.google.com/advanced_search')
             .setValue(mainQueryInputSelector, mainQuery)
             .click(langDropdownSelector)
             .click(langDropdownValueSelector)
-            .saveScreenshot('tests_output/google.png')
+            .click(lastUpdateDropdownSelector)
+            .click(lastUpdateDropdownValueSelector)
+            .click(submitButtonSelector)
+            .assert.urlContains('as_q=Elon+Musk', 'Query is Elon Musk')
+            .assert.urlContains('lr=lang_it', 'Language is Italian')
+            .assert.urlContains('as_qdr=m', 'Time period is last month')
+
+        browser.assert.visible(resultsPageQuerySelector, 'UI: Elon Musk is set in the query input');
+        browser.assert.containsText(resultsPageLangSelector, 'Search Italian pages', 'UI: Language is set to Italian');
+        browser.assert.containsText(resultsPageLastUpdateSelector, 'Past month', 'UI: Last update is set to Past Month');
+
+        browser.saveScreenshot('tests_output/google.png')
 
     }
 }
